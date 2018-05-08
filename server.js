bf472e1f8ec4e2168aa3c4cec826cb4ec4f95ca5
Trevor Johnson <johnsontrevor55@gmail.com>
2018-05-07 19:20:55 -0700

    added routes folder

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
+var db = require('./models');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +16,6 @@ app.use(bodyParser.json());
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log('App listening on PORT ' + PORT);
   });
 });
